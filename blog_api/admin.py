af207b8e03a7c453f053b6483558c6a17b515b8a
Harris <harriskarume.hk@gmail.com>
2023-04-16 17:43:29 +0300

    Register category

--- blog_api/admin.py
+++ blog_api/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'description')
+
+admin.site.register(models.Category, CategoryAdmin)
