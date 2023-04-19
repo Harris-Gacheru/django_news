69d613ca37530f11c087d5b34f4895e66040d50b
Harris <harriskarume.hk@gmail.com>
2023-04-19 08:39:27 +0300

    Include

--- blog_api/admin.py
+++ blog_api/admin.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
 from . import models
 
-class CategoryAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name', 'description')
+# class CategoryAdmin(admin.ModelAdmin):
+#     list_display = ('id', 'name', 'description')
 
-class ArticleAdmin(admin.ModelAdmin):
-    list_display = ('id', 'title', 'published_at', 'author_id', 'category')
+# class ArticleAdmin(admin.ModelAdmin):
+#     list_display = ('id', 'title', 'published_at', 'author_id', 'category')
 
-list_models = [models.Article, models.Category]
-admin.site.register(list_models)
+# list_models = [models.Article, models.Category]
+admin.site.register(models.Article)
+admin.site.register(models.Category)
